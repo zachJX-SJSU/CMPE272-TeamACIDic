1ba56709eea5b11209011fa1822b29bfed9f8d0c
unknown <jxie2@andrew.cmu.edu>
2025-09-26 21:09:42 -0700

    add POST and GET for issues in main.py

--- HW2_GitProxy/app/main.py
+++ HW2_GitProxy/app/main.py
@@ -28,3 +28,54 @@ async def _startup():
             logger.info("startup_test_list_issues", message="No open issues found")
     except Exception as e:
         logger.error("startup_test_list_issues_failed", error=str(e))
+
+
+# 1) POST /issues, create new issues
+# Author: Zach Xie
+@app.post("/issues", response_model=Issue, status_code=201, responses={400: {"model": ErrorResponse}, 401: {"model": ErrorResponse}})
+async def create_issue(req: CreateIssueRequest, response: Response, request: Request):
+    payload = req.model_dump(exclude_none=True)
+    now = datetime.now(timezone.utc).isoformat()
+    logger.debug("Calling POST /issues ...", timestamp=now)
+    try:
+        data, headers = await client.create_issue(payload)
+        response.headers.update(headers)
+        response.headers["Location"] = f"/issues/{data['number']}"
+
+        # Log successful response
+        issue_number = data['number']
+        logger.info("Issue created!",
+            timestamp=now,
+            issue_number=issue_number,
+        )
+        return data
+    except HTTPException as e:
+        # Log failed response
+        logger.error(f"Issues creation failed with {e.status_code}",
+            timestamp=now,
+        )
+        if e.status_code == 401:
+            return JSONResponse(status_code=401, content={"error": "unauthorized", "details": e.detail})
+        raise
+
+# 2) GET /issues, get all issues
+# Author: Zach Xie
+@app.get("/issues", response_model=list[Issue])
+async def list_issues(state: str = "open", labels: str | None = None, page: int = 1, per_page: int = 30, response: Response = None):
+    now = datetime.now(timezone.utc).isoformat()
+    logger.debug("Calling GET /issues with param ...", 
+        timestamp=now,
+        state=state,
+        page=page,
+        per_page=per_page,
+        labels=labels
+    )
+    if per_page > 100:
+        raise HTTPException(status_code=400, detail={"error": "per_page must be <= 100"})
+    params = {"state": state, "page": page, "per_page": per_page}
+    if labels:
+        params["labels"] = labels
+    data, headers = await client.list_issues(params)
+    if response:
+        response.headers.update(headers)
+    return data
